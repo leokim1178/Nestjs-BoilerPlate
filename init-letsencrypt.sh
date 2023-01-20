5a422689d386a003f2adba8a98dfa995cd0a1a3e
leokim1178 <leo950906@gmail.com>
2023-01-20 17:48:36 +0900

    🔧 FIX /  init-letsencrypt 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker compose)" ]; then
-  echo 'Error: docker compose is not installed.' >&2
-  exit 1
-fi
+# if ! [ -x "$(command -v docker compose)" ]; then
+#   echo 'Error: docker compose is not installed.' >&2
+#   exit 1
+# fi
 
 domains="leo-boiler.store"
 rsa_key_size=4096
