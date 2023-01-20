b8c1f15dd34d680e66aec98bae55db3a4dd6932e
leokim1178 <leo950906@gmail.com>
2023-01-20 17:47:12 +0900

    🔧 FIX /  init-letsencrypt 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains="도메인 주소"
+domains="leo-boiler.store"
 rsa_key_size=4096
 data_path="../certbot"
-email="이메일 주소" # Adding a valid address is strongly recommended
+email="leo3179@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
