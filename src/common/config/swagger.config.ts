49493d073c6c013c31312798c4e9e8b95a35e10d
LeoKim <leo3179@naver.com>
2023-02-05 12:24:31 +0900

    ✨ FEAT / 스웨거 설치

---
+++ src/common/config/swagger.config.ts
@@ -0,0 +1,14 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerOptions = new DocumentBuilder()
+  .setTitle('NestJS Boilerplate')
+  .setDescription('NestJS Boilerplate API description')
+  .setVersion('1.0')
+  .addTag('nestjs-boilerplate')
+  .addBearerAuth({
+    type: 'http',
+    scheme: 'bearer',
+    description:
+      'JWT Authorization header using the Bearer scheme. Example: "bearer {token}',
+  })
+  .build();
